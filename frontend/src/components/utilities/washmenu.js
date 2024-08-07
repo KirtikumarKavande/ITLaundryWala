eebbdbaf436a4c451042a8bc805eac04328c14b2
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-08-07 10:05:12 +0530

    removed some files from git ignore

---
+++ frontend/src/components/utilities/washmenu.js
@@ -0,0 +1,52 @@
+export const washMenu = [
+  {
+    item: " WASH & FOLD",
+    price: 50,
+    type: "perKg",
+    key:"W & F"
+  },
+  {
+    item: "WASH & IRON",
+    price: 60,
+    type: "perKg",
+    key:"W & I"
+
+  },
+
+  {
+    item: "DRY CLEANING",
+    price: 0,
+    type: "perPiece",
+    key:"DC"
+
+  },
+  {
+    item: "PRESS",
+    price: 0,
+    type: "perPiece",
+    key:"PRESS"
+
+  },
+  {
+    item: "HOUSEHOLD",
+    price: 0,
+    type: "perPiece",
+    key:"H-H"
+
+  },
+
+  {
+    item: "SHOES",
+    price: 0,
+    type: "perPiece",
+    key:"SHOES"
+
+  },
+  {
+    item: "EXPRESS LAUNDRY",
+    price: 0,
+    type: "perPiece",
+    key:"SHOES"
+
+  },
+];
