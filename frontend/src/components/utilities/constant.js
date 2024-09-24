971bed90951b3290a22e8afe4b6a69ec01d0dd72
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-24 09:22:17 +0530

    development: 2

--- frontend/src/components/utilities/constant.js
+++ frontend/src/components/utilities/constant.js
@@ -1,7 +1,7 @@
 import { Smile, Gauge, Leaf, Clock } from 'lucide-react';
 
-export const BASE_URL="http://localhost:4000"
-// export const BASE_URL="http://13.201.148.14:4000"
+// export const BASE_URL="http://localhost:4000"
+export const BASE_URL="http://13.201.148.14:4000"
 export const NUMBER_OF_CLOTH_TYPE=[1,2,3,4,5]
 export const MonthNames=['Jan','Feb','March','April','May','June','July','Aug','Sep','Oct','Nov','Dec']
 export const PICKUP_TIME=["Select","07.00 AM - 10.00 AM","12.00 PM - 3.00 PM","06.00 PM - 9.00 PM"]
